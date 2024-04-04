c09366597246304ee803647a992b04308764c2d7
LucasW20 <lucas.wright552@gmail.com>
2024-04-03 20:00:14 -0500

    Components, Images, and Navigation

---
+++ client/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Header (props) {
+  return (
+    <div className="row">
+      <div className="pb-2 mt-4 mb-2 border-bottom" style={{ width: '100%' }}>
+
+      </div>
+    </div>
+  )
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import Express from 'express'
 
-//Local server. Only used for testing.
+//Local server. Only used for testing on your local machine with the server:dev script.
+//Push changes to github after testing. Make sure to copy/move files in public to the root folder so the
+// github page displays correctly.
 
 const app = new Express()
 
@@ -11,6 +13,6 @@ app.use((req, res, next) => {
 
 app.use(Express.static('public'))
 
-app.listen(5000, () => {
-  console.log('Listening on port 5000')
+app.listen(5001, () => {
+  console.log('Listening on port 5001')
 })
